e86fe559d28a534d91d1bca969695f2a3119b086
Joaquin Montecino <joaquinmontecino13@gmail.com>
2023-11-22 04:52:44 -0300

    minor fix v2

--- db_apis/convenios.js
+++ db_apis/convenios.js
@@ -105,7 +105,7 @@ module.exports.create = create;
 
 const updateSql =
  `BEGIN
-    UPDATE_CONVENIO(:id_convenio,:id_unidad_gestora,:nombre_conv,:tipo_conv,:movilidad,:vigencia,:ano_firma,:tipo_firma,:cupos,:documentos,:condicion_renovacion,:estatus,:fecha_inicio,:fecha_termino);
+    UPDATE_CONVENIO(:id_convenio,:nombre_conv,:tipo_conv,:movilidad,:vigencia,:ano_firma,:tipo_firma,:cupos,:documentos,:condicion_renovacion,:estatus,:fecha_inicio,:fecha_termino);
   END;`;
 
 
